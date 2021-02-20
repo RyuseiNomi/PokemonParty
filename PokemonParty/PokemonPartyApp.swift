e69d08f1ab6d6bfc8fddcd4a10372aefc9fac92f
RyuseiNomi <kanontonto1010@gmail.com>
2021-02-20 11:28:20 +0900

    Initial Commit

---
+++ PokemonParty/PokemonPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonPartyApp.swift
+//  PokemonParty
+//
+//  Created by 能美龍星 on 2021/02/20.
+//
+
+import SwiftUI
+
+@main
+struct PokemonPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonPartyTests/PokemonPartyTests.swift
@@ -0,0 +1,33 @@
+//
+//  PokemonPartyTests.swift
+//  PokemonPartyTests
+//
+//  Created by 能美龍星 on 2021/02/20.
+//
+
+import XCTest
+@testable import PokemonParty
+
+class PokemonPartyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
